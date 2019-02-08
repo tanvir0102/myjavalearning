7edb66accc6d487794215f5f1207a734af6d5bd9
Md Tanvir <tanvir0102@gmail.com>
2019-02-09 01:27:29 +0530

    ByteShortInt

---
+++ src/com/devopadda/udemytim/ByteShortInt.java
@@ -0,0 +1,15 @@
+package com.devopadda.udemytim;
+
+public class ByteShortInt {
+
+	public static void main(String[] args) {
+		byte byteValue = 10;
+		short shortValue = 20;
+		int intValue = 50;
+		
+		long longTotal = 5000L + 10L * (byteValue + shortValue + intValue);
+		System.out.println("longTotal = " + longTotal);
+
+	}
+
+}
--- src/com/devopadda/udemytim/VariablesDatatypesOperators.java
+++ src/com/devopadda/udemytim/VariablesDatatypesOperators.java
@@ -6,8 +6,17 @@ public class VariablesDatatypesOperators {
 		
 		System.out.println("Hello Tanvir, Continue learning Java...");
 		
-		int myFirstNumber = 5;
-		System.out.println(myFirstNumber);
+		int myFirstNumber = (10 + 5) + (2 * 10);
+		int mySecondNumber = 12;
+		int myThirdNumber = myFirstNumber * 2;
+		
+		int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+		
+		int myLastOne = 1000 - myTotal;
+		
+		System.out.println(myLastOne);
+		
+		
 		
 
 	}
